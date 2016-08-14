7fa132ca89582653783c5462e84f330d3ec6d6f4
lalitc375 <lalitc375@gmail.com>
2016-08-14 09:41:20 +0530

    finishing

---
+++ myapp/static/myapp/loginCtrl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lalitnitrr on 14/8/16.
+ */
+ var mainApp=angular.module('mainApp', ['ngRoute']);
+
+
+ mainApp.controller('loginCtrl', function($scope,$http) {
+     $scope.onClickLogin=function () {
+     }
+
+
+ });
---
+++ myapp/static/myapp/signUpCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lalitnitrr on 14/8/16.
+ */
+ var mainApp=angular.module('mainApp', []);
+
+ mainApp.controller('signUpCtrl', function($scope,$http) {
+
+ });
